367f80ca351818d6732f5c96f0ca807741e77201
Chance <chancesmithb@gmail.com>
2022-03-08 22:26:21 -0600

    💅  STYLE: [signup] add secret code

--- app/auth/mutations/signup.ts
+++ app/auth/mutations/signup.ts
@@ -3,7 +3,11 @@ import db from "db"
 import { Signup } from "app/auth/validations"
 import { Role } from "types"
 
-export default resolver.pipe(resolver.zod(Signup), async ({ email, password }, ctx) => {
+export default resolver.pipe(resolver.zod(Signup), async ({ email, password, secretCode }, ctx) => {
+  // if secretCode does not match, then throw error
+  if (secretCode !== process.env.SECRET_CODE || !secretCode) {
+    throw new Error("Invalid secret code")
+  }
   const hashedPassword = await SecurePassword.hash(password.trim())
   const user = await db.user.create({
     data: { email: email.toLowerCase().trim(), hashedPassword, role: "USER" },
