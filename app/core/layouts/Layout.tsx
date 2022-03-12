c02e36bdf6e4d161d427ed7345f4a82090ae365f
Chance <chancesmithb@gmail.com>
2022-03-11 22:49:30 -0600

    📦  FEAT: [layout] add menu

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -1,6 +1,16 @@
-import { Head, BlitzLayout, Script, Image } from "blitz"
+import { Head, BlitzLayout, Script, Image, Link, useRouter } from "blitz"
+import { useState } from "react"
 
 const Layout: BlitzLayout<{ title?: string }> = ({ title, children }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const router = useRouter()
+
+  function handleOpenMenu() {
+    setIsMenuOpen(true)
+  }
+  function handleCloseMenu() {
+    setIsMenuOpen(false)
+  }
   return (
     <>
       <Head>
@@ -19,14 +29,69 @@ const Layout: BlitzLayout<{ title?: string }> = ({ title, children }) => {
             />
           </a>
         </div>
-        {/* <div className="menu">
+        <div className="menu" onClick={handleOpenMenu}>
           <i className="fal fa-bars" />
-        </div> */}
+        </div>
       </header>
 
       <div className={`${title === "All Meetings" ? "" : "layout-wrapper layout-other"}`}>
         {children}
       </div>
+      <div className={`c-menu ${isMenuOpen ? "c-menu__open" : ""}`}>
+        <div className="c-menu__close" onClick={handleCloseMenu}>
+          <i className="fa fa-times" />
+        </div>
+        <ul>
+          <li
+            onClick={handleCloseMenu}
+            className={`c-menu__item ${router.asPath === "/" ? "c-menu__item--active" : ""}`}
+          >
+            <Link href="/">Meetings</Link>
+          </li>
+          <li
+            onClick={handleCloseMenu}
+            className={`c-menu__item ${
+              router.asPath === "/resources" ? "c-menu__item--active" : ""
+            }`}
+          >
+            <Link href="/resources">Resources</Link>
+          </li>
+        </ul>
+      </div>
+      <style jsx>{`
+        .c-menu {
+          display: none;
+          position: absolute;
+          height: 100vh;
+          top: 0;
+          right: 0;
+          background: #fff;
+          padding: 1rem 125px 1rem 1rem;
+        }
+        .c-menu a {
+          text-decoration: none !important;
+        }
+        .c-menu ul {
+          list-style-type: none;
+          padding: 0.5rem 0 0 1rem;
+        }
+        .c-menu ul li,
+        .c-menu__item {
+          padding: 1rem 0 0 0;
+        }
+        .c-menu__item--active {
+          font-weight: bold;
+        }
+        .c-menu__open {
+          display: block;
+        }
+        .c-menu__close {
+          position: absolute;
+          right: 1rem;
+          top: 1rem;
+          color: var(--primary);
+        }
+      `}</style>
     </>
   )
 }
