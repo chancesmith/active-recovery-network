ca5da7c6978620f56094eac880cb193534bb9271
Chance <chancesmithb@gmail.com>
2022-03-11 22:59:32 -0600

    💅  STYLE: [resources] adjust background and header

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -18,7 +18,7 @@ const Layout: BlitzLayout<{ title?: string }> = ({ title, children }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Script src="https://kit.fontawesome.com/1f3310818b.js" crossOrigin="anonymous" />
-      <header className="header">
+      <header className={`${title === "All Meetings" ? "" : "header"}`}>
         <div className="logo">
           <a href="/">
             <Image
@@ -37,6 +37,7 @@ const Layout: BlitzLayout<{ title?: string }> = ({ title, children }) => {
       <div className={`${title === "All Meetings" ? "" : "layout-wrapper layout-other"}`}>
         {children}
       </div>
+
       <div className={`c-menu ${isMenuOpen ? "c-menu__open" : ""}`}>
         <div className="c-menu__close" onClick={handleCloseMenu}>
           <i className="fa fa-times" />
